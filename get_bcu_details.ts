73408fa558b3c13b7bd364e24fd6823edeafc43a
EduAir94 <49965850+EduAir94@users.noreply.github.com>
2023-01-03 21:23:19 -0300

    fix: check retry attempts

--- get_bcu_details.ts
+++ get_bcu_details.ts
@@ -4,12 +4,13 @@ import BCU_Details from "./classes/bcu_details";
 import { MongooseServer } from "./classes/database";
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 axiosRetry(axios, {
-  retries: 3, // number of retries
-  retryDelay: (retryCount) => {
+  retries: 10, // number of retries
+  retryDelay: (retryCount: number) => {
     console.log(`retry attempt: ${retryCount}`);
     return retryCount * 2000; // time interval between retries
   },
   retryCondition: (error) => {
+    console.log("Status Error", error.response.status);
     // if retry condition is not specified, by default idempotent requests are retried
     return error.response.status === 503 || error.response.status === 502;
   },
