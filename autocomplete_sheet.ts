5924f0fbf149453a413f2dce8d7ca0b61e88a339
Eduardo Airaudo <eduardo@optimmus.io>
2023-01-08 22:22:35 -0300

    fix: sleep request row save

--- autocomplete_sheet.ts
+++ autocomplete_sheet.ts
@@ -1,6 +1,7 @@
 import { GoogleSpreadsheet, GoogleSpreadsheetRow } from "google-spreadsheet";
 import { cambio_info } from "./classes/cambioInfo";
 import { MongooseServer } from "./classes/database";
+import { sleep } from "./config/config";
 import * as credentials from "./sheet_key.json";
 
 async function main() {
@@ -10,20 +11,27 @@ async function main() {
   await document.loadInfo();
   const sheet = document.sheetsByIndex[0];
   const rows: GoogleSpreadsheetRow[] = await sheet.getRows();
+  let pos = 1;
+  const totalRows = rows.length;
   for (let row of rows) {
+    console.log(pos, totalRows);
     const id = row.ID;
     console.log("ID", id);
     const findSuc: any = await cambio_info.findSuc(id);
     if (findSuc) {
       const phone = findSuc.Telefono;
       const name = findSuc.Nombre;
-      if (phone) row.Telefono = phone;
-      if (name) row.Nombre = name;
-      console.log("Row", row);
-      await row.save();
+      if (!row.Telefono && !row.Nombre) {
+        if (phone) row.Telefono = phone;
+        if (name) row.Nombre = name;
+        console.log("Row", row);
+        await row.save();
+        await sleep(500);
+      }
     } else {
       console.log("No suc", id);
     }
+    pos++;
   }
   console.log("FINISH");
 }
