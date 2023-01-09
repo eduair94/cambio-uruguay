90fb2b226f6ddc427bb57e06424006d5bc1a8901
Eduardo Airaudo <eduardo@optimmus.io>
2023-01-08 22:10:49 -0300

    feat: autocomplete sheet

---
+++ autocomplete_sheet.ts
@@ -0,0 +1,27 @@
+import { GoogleSpreadsheet, GoogleSpreadsheetRow } from "google-spreadsheet";
+import { cambio_info } from "./classes/cambioInfo";
+import { MongooseServer } from "./classes/database";
+import * as credentials from "./sheet_key.json";
+
+async function main() {
+  await MongooseServer.startConnectionPromise();
+  const document = new GoogleSpreadsheet("1yKfUC3EZbpiFD-6yJuoUewgjjzA2yv9zhy7a0G2zD30");
+  await document.useServiceAccountAuth(credentials);
+  await document.loadInfo();
+  const sheet = document.sheetsByIndex[0];
+  const rows: GoogleSpreadsheetRow[] = await sheet.getRows();
+  for (let row of rows) {
+    const id = row.ID;
+    const findSuc: any = await cambio_info.findSuc(id);
+    if (findSuc) {
+      const phone = findSuc.Telefono;
+      const name = findSuc.Nombre;
+      if (phone) row.Telefono = phone;
+      if (name) row.Nombre = name;
+      await row.save();
+    }
+  }
+  console.log("FINISH");
+}
+
+main();
