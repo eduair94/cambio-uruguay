46d2399dd766f5023d3a527fcbb0b41646cdfc64
Eduardo Airaudo <eduardo@optimmus.io>
2023-07-30 17:56:27 -0300

    fix cambio uy

--- classes/cambios/prex.ts
+++ classes/cambios/prex.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { load } from "cheerio";
 import dotenv from "dotenv";
 import { CambioObj } from "../../interfaces/Cambio";
 import { Cambio } from "../cambio";
@@ -14,49 +15,84 @@ class CambioPrex extends Cambio {
     const url = 'https://www.prexcard.com/api/login';
     const headers = {
       authorization: 'Bearer TokenAPP011001010',
+      'accept-charset': 'UTF-8',
+      'accept-encoding': 'gzip',
+      connection: 'Keep-Alive',
+      'content-type': 'application/json',
+      'device-app-version': '10.49.45',
+      'device-manufacturer': 'Xiaomi',
+      'device-model': 'M2102J20SG',
+      'device-platform': 'Android',
+      'device-serial': 'unknown',
+      'device-uuid': 'dd600187f3582fa7',
+      'device-version': '11',
+      host: 'www.prexcard.com',
+      'user-agent': 'Dalvik/2.1.0 (Linux; U; Android 11; M2102J20SG Build/RKQ1.200826.002)'
     }
     const json = {
-      "Documento": e.prex_doc,
-      "Password": e.prex_pass,
-      "TipoDocumento": "CI",
-      "TipoPersona": "1",
-      "UuidDevice": "dd600127f3582fa7",
-      "ModelDevice": "M2102220SG",
-      "VersionDevice": "11",
-      "SerialDevice": "unknown",
-      "PlatformDevice": "Android",
-      "ManufacturerDevice": "Xaaomi",
-      "TokenFCM": "",
-      "AppVersion": "10.49.44"
-    };
-    const res = await axios.post(url, json, { headers }).then(res=>res.data);
-    return res.token;
+    "Documento": "airaudoeduardo@gmail.com",
+    "Password": "Ou$QLFr9",
+    "TipoDocumento": "CI",
+    "TipoPersona": "1",
+    "UuidDevice": "dd600187f3582fa7",
+    "ModelDevice": "M2102J20SG",
+    "VersionDevice": "11",
+    "SerialDevice": "unknown",
+    "PlatformDevice": "Android",
+    "ManufacturerDevice": "Xiaomi",
+    "TokenFCM": "",
+    "AppVersion": "10.49.45"
+  }
+    const res = await axios.post(url, json, { headers }).then(res => res.data).catch(e => {
+      console.log("Error", url);
+      console.error(e)
+    });
+    return e.token;
   }
   async prex_ar(token:string) {
-    const url = "https://www.prexcard.com/api/prex_a_prex_internacional_get_cotizacion_pais";
+    const url = "https://www.prexcard.com/hacelabien";
     const headers = {
-      Authorization: `Bearer ${token}`,
-    };
-    const res = await axios.post(url, { usuario_id: e.prex_user_id.toString(), pais_id: 32 }, { headers }).catch((e) => {
+      "User-Agent":
+        "Dalvik/2.1.0 (Linux; U; Android 7.1.2; SM-N976N Build/N2G48H)",
+    }
+    const res = await axios.get(url, { headers }).catch((e) => {
       console.error(e);
       return {
         data: null,
       };
     });
     const d = res.data;
-    return d;
+    const $ = load(d);
+    const cotArg = parseFloat($("#cotizacionArg").attr('value'));
+    const cotUy = parseFloat($("#cotizacionUy").attr('value'));
+    return {
+      cotArg,
+      cotUy
+    };
   }
-  async get_usd() {
+  async get_usd(token:string) {
     const url = "https://www.prexcard.com/api/cotizacion_usd";
     const header = {
+      "Accept-Charset": "UTF-8",
+      Authorization: `Bearer ${token}`,
+      "Device-Serial": "2070937402d119c1",
+      "Device-Manufacturer": "samsung",
+      "User-Agent":
+        "Dalvik/2.1.0 (Linux; U; Android 7.1.2; SM-N976N Build/N2G48H)",
+      Host: "www.prexcard.com",
+      "Accept-Encoding": "gzip",
     };
-    const body = null;
-    const res = await axios.post(url, body, { headers: header }).then((res) => res.data);
-    console.log("Response", res);
+    const body = {};
+    const res = await axios
+      .post(url, body, { headers: header })
+      .then((res) => res.data);
     return { buy: res.compra, sell: res.venta };
   }
   async get_data(): Promise<CambioObj[]> {
-    const { buy, sell } = await this.get_usd();
+    const token = await this.login();
+    const ar = await this.prex_ar(token);
+    console.log("AR", ar);
+    const { buy, sell } = await this.get_usd(token);
     const f = [
       {
         code: "USD",
@@ -66,8 +102,7 @@ class CambioPrex extends Cambio {
         sell: sell,
       },
     ];
-    const token = await this.login();
-    const ar = await this.prex_ar(token);
+    console.log(f);
     let arF = null;
     console.log("Ar", ar);
     if (ar) {
@@ -75,8 +110,8 @@ class CambioPrex extends Cambio {
         code: "ARS",
         type: "",
         name: "",
-        buy: ar.cotizaciones.UY.compra / ar.cotizaciones.AR.venta,
-        sell: ar.cotizaciones.UY.venta / ar.cotizaciones.AR.compra,
+        buy: Math.round(ar.cotUy / ar.cotArg * 100) / 100,
+        sell: "",
       };
     }
     if (arF) {
