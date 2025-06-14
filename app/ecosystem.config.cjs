799a7122976e485f1ded251dca25f9b97d4d8648
eduair94 <49965850+eduair94@users.noreply.github.com>
2025-06-14 03:17:31 -0300

    fix: port cambio uruguay

--- app/ecosystem.config.cjs
+++ app/ecosystem.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'cambio-uruguay',
+      port: '3311',
       exec_mode: 'cluster',
       instances: '2', // Or a number of instances
       script: './.output/server/index.mjs',
