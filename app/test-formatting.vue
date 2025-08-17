c095a6d071b25b672432221a97cdde6a28509195
eduair94 <49965850+eduair94@users.noreply.github.com>
2025-08-17 17:24:57 -0300

    Test pre-commit hook

---
+++ app/test-formatting.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-icon   
+    size="64"   
+    color="grey-lighten-1"   >mdi-test</v-icon>
+    <p  class="test" >Test content {{ _testVar }} {{ _anotherVar }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const _testVar='test'
+const _anotherVar=    'another'
+</script>
