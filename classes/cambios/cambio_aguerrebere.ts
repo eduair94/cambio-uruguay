ae72d657dbb56bdccbac4ce2b8b9054622eaf38b
eduair94 <49965850+eduair94@users.noreply.github.com>
2025-11-06 03:29:20 -0300

    fix. cambio aguerrebere

--- classes/cambios/cambio_aguerrebere.ts
+++ classes/cambios/cambio_aguerrebere.ts
@@ -7,74 +7,80 @@ class CambioAguerrebere extends Cambio {
   name = "Cambio Aguerrebere";
   bcu = "https://www.bcu.gub.uy/Servicios-Financieros-SSF/Paginas/InformacionInstitucion.aspx?nroinst=2370";
   private conversions = {
-    Dolar: {
+    USD: {
       code: "USD",
       type: "",
+      name: "Dolar",
     },
-    Peso: {
+    ARS: {
       code: "ARS",
       type: "",
+      name: "Peso",
     },
-    Real: {
+    BRL: {
       code: "BRL",
       type: "",
+      name: "Real",
     },
-    Euro: {
+    EUR: {
       code: "EUR",
       type: "",
+      name: "Euro",
     },
   };
   website = "https://cambioaguerrebere.com/";
   favicon = "https://cambioaguerrebere.com/";
   async get_data(): Promise<CambioObj[]> {
-    const web_data = await axios.get("https://cambioaguerrebere.com/").then((res) => res.data);
+    const web_data = await axios.get("https://cambioaguerrebere.com/", {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'es-UY,es;q=0.9,en;q=0.8',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1'
+      }
+    }).then((res) => res.data);
     const $ = load(web_data);
 
     // Find the exchange rate container
     const container = $(".container .row");
 
-    // Extract currency names from the first column (excluding header)
+    // Extract currency codes from .currency-name spans in the first column
     const currencies = container
-      .find(".col:first-child .value")
+      .find(".col:first-child .currency-name, .col:first-child .currency-name-eur")
       .map((i, el) => $(el).text().trim())
       .get();
 
-    // Extract buy rates from the second column (COMPRA) (excluding header)
+    // Extract buy rates from the second column (COMPRA)
     const buyRates = container
       .find(".col:nth-child(2) .value")
       .map((i, el) => $(el).text().trim())
-      .get();
+      .get()
+      .filter(val => val && val !== "COMPRA"); // Filter out header
 
-    // Extract sell rates from the third column (VENTA) (excluding header)
+    // Extract sell rates from the third column (VENTA)
     const sellRates = container
       .find(".col:nth-child(3) .value")
       .map((i, el) => $(el).text().trim())
-      .get();
-
-    // Map currency codes to the expected format
-    const currencyMapping: { [key: string]: string } = {
-      USD: "Dolar",
-      ARS: "Peso",
-      BRL: "Real",
-      EUR: "Euro",
-    };
+      .get()
+      .filter(val => val && val !== "VENTA"); // Filter out header
 
     const result: CambioObj[] = [];
 
     for (let i = 0; i < currencies.length; i++) {
       const currencyCode = currencies[i];
-      const currencyName = currencyMapping[currencyCode];
-      if (currencyName && this.conversions[currencyName] && buyRates[i] && sellRates[i]) {
+      
+      if (this.conversions[currencyCode] && buyRates[i] && sellRates[i]) {
         // Remove $ symbol before calling fix_money
         const buyRate = this.fix_money(buyRates[i].replace("$", ""));
         const sellRate = this.fix_money(sellRates[i].replace("$", ""));
 
         if (buyRate && sellRate) {
-          const { code, type } = this.conversions[currencyName];
+          const { code, type, name } = this.conversions[currencyCode];
           result.push({
             code,
             type,
-            name: currencyName,
+            name,
             buy: buyRate,
             sell: sellRate,
           });
--- test_geo.ts
+++
@@ -1,5 +0,0 @@
-import geocoder from "geocoder";
-geocoder.selectProvider("geonames", { username: "EduAir", pass: "j?NjL&5DQ?pcyf5$" });
-geocoder.geocode("Atlanta, GA", function (err, data) {
-  console.log(err, data);
-});
